654b557e15dc46b732eecc725c73c275a19d93a8
jesusmbonillaf-art <jesusmbonillaf@gmail.com>
2026-06-27 15:03:13 -0500

    Inventario 8.2

--- obj/Debug/net10.0/EF FUNDAMENTO.AssemblyInfo.cs
+++ obj/Debug/net10.0/EF FUNDAMENTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF FUNDAMENTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1f503cc68a9b7f42d5f7051e1e51714ad1327c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2c3b61a2b388f3a0e5c3ac90a6eb2330bb8c40")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF FUNDAMENTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF FUNDAMENTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
